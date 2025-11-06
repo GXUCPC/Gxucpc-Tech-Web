da9e6162dedff5af2050f7284b5ca88884a0302a
寰辰 <2180945240@qq.com>
2025-11-06 23:57:39 +0800

    feat：主页简介部分动画

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,35 +1,234 @@
 <script setup lang="ts">
 import { useGlobalLoading } from '@/store/globalLoading.ts'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import { SplitText } from 'gsap/SplitText'
+import { onMounted, useTemplateRef } from 'vue'
 
 const globalLoading = useGlobalLoading()
+
+function infoSectionAni(ele: HTMLDivElement | null) {
+  const tl = gsap.timeline()
+  if (!ele) return tl
+  //竖线
+  tl.fromTo(
+    ele.querySelector('.line > div'),
+    {
+      height: '0%',
+    },
+    {
+      height: '100%',
+      ease: 'sine.out',
+      duration: 2,
+    },
+  )
+  tl.from(
+    ele.querySelector('.el-image'),
+    {
+      autoAlpha: 0,
+      duration: 1,
+    },
+    '<',
+  )
+
+  //简介文字动画
+  SplitText.create(ele.querySelector('.text1'), {
+    type: 'chars,words',
+    autoSplit: true,
+    mask: 'chars',
+    onSplit: (self) => {
+      tl.from(
+        self.words,
+        {
+          duration: 1,
+          autoAlpha: 0,
+          stagger: 0.05,
+        },
+        '<',
+      )
+    },
+  })
+  SplitText.create(ele.querySelector('.text2'), {
+    type: 'chars,words',
+    autoSplit: true,
+    mask: 'chars',
+    onSplit: (self) => {
+      tl.from(
+        self.words,
+        {
+          duration: 1,
+          autoAlpha: 0,
+          stagger: 0.05,
+        },
+        '<0.5',
+      )
+    },
+  })
+  SplitText.create(ele.querySelector('.text3'), {
+    type: 'lines',
+    autoSplit: true,
+    mask: 'chars',
+    onSplit: (self) => {
+      tl.from(
+        self.lines,
+        {
+          y: '2em',
+          duration: 1,
+          autoAlpha: 0,
+          stagger: 0.2,
+        },
+        '<0.5',
+      )
+    },
+  })
+
+  return tl
+}
+
+const headTextRef = useTemplateRef('headText')
+const icpcInfoRef = useTemplateRef('icpcInfo')
+const icpcTechInfoRef = useTemplateRef('icpcTechInfo')
+onMounted(() => {
+  gsap.registerPlugin(SplitText, ScrollTrigger)
+  const headTextEle = headTextRef.value
+  if (!headTextEle) return
+  const tl = gsap.timeline()
+  //初始动画
+  tl.from(headTextEle.querySelector('.item1')?.children!, {
+    delay: 3,
+    duration: 0.5,
+    yPercent: 100,
+    autoAlpha: 0,
+    stagger: 1,
+  })
+    .from(headTextEle.querySelector('.item2'), {
+      duration: 0.5,
+      yPercent: 100,
+      autoAlpha: 0,
+    })
+    .fromTo(
+      headTextEle.querySelector('.item3'),
+      {
+        autoAlpha: 0,
+      },
+      {
+        duration: 0.5,
+        autoAlpha: 1,
+      },
+    )
+    // 默认显示第一个信息
+    .add(infoSectionAni(icpcInfoRef.value))
+
+  ScrollTrigger.create({
+    trigger: icpcTechInfoRef.value,
+    animation: infoSectionAni(icpcTechInfoRef.value),
+  })
+})
 </script>
 
 <template>
-  <section>
-    <h2>集训队简介</h2>
-    <p>
-      广西大学ICPC集训队（ICPCTrainingTeamofChina,GuangxiUniversity）于2016年建立，2018-2019年开始逐渐正式运转，是一个为了代表广西大学参加以国际大学生程序设计竞赛（ICPC）、中国大学生程序设计竞赛（CCPC）等高水平赛事、希望与国内绝大部分院校接轨从而进行严格集中训练的队伍。集训队并非社团，仅为学生兴趣同好组织。集训队目前名义上属于计算机与电子信息学院计算机协会。集训队由计算机与电子信息学院以学院的名义提供经费和场地支持，而集训队为学院以及学校提供程序设计竞赛（算法竞赛）和相关课程的支持、管理与维护。集训队分为正式队伍和技术组：正式队员参加各地举行的算法竞赛，技术组为竞赛队员提供后勤服务，保障竞赛队员的训练正常开展，同时学习项目开发的前沿知识，参与队内开发活动。
-    </p>
-    <p class="p_1">集训队并非社团，仅为学生兴趣同好组织。</p>
+  <section class="headText" ref="headText">
+    <div class="item1">
+      <span class="keyword">算竞</span> <span>还是</span> <span class="keyword">开发</span>
+    </div>
+    <div class="item2">总有一个适合你的</div>
+    <el-button class="item3" type="primary">了解更多</el-button>
+  </section>
 
-    <p>
-      集训队目前名义上属于计算机与电子信息学院计算机协会。集训队由计算机与电子信息学院以学院的名义提供经费和场地支持，而集训队为学院以及学校提供程序设计竞赛（算法竞赛）和相关课程的支持、管理与维护。
-    </p>
+  <section class="icpc infoContainer" ref="icpcInfo">
+    <div class="line">
+      <div></div>
+    </div>
+    <div class="info">
+      <div class="infoBrief">
+        <el-image class="item1" style="width: 60px; height: 60px" />
+        <div style="display: flex; flex-direction: column; gap: 5px">
+          <div class="text1">广西大学ICPC集训队</div>
+          <div class="text2">ICPC Training Team of China, Guangxi University</div>
+        </div>
+      </div>
+      <div class="text3" style="margin-top: 2em; display: flex; flex-direction: column; gap: 0.5em">
+        <p>
+          广西大学ICPC集训队（ICPC Training Team of China, Guangxi
+          University）于2016年建立，2018-2019年开始逐渐正式运转，是一个为了代表广西大学参加以国际大学生程序设计竞赛（ICPC）、中国大学生程序设计竞赛（CCPC）等高水平赛事、希望与国内绝大部分院校接轨从而进行严格集中训练的队伍。集训队并非社团，仅为学生兴趣同好组织。集训队目前名义上属于计算机与电子信息学院计算机协会。集训队由计算机与电子信息学院以学院的名义提供经费和场地支持，而集训队为学院以及学校提供程序设计竞赛（算法竞赛）和相关课程的支持、管理与维护。集训队分为正式队伍和技术组：正式队员参加各地举行的算法竞赛，技术组为竞赛队员提供后勤服务，保障竞赛队员的训练正常开展，同时学习项目开发的前沿知识，参与队内开发活动。
+        </p>
+        <p>集训队并非社团，仅为学生兴趣同好组织。</p>
+
+        <p>
+          集训队目前名义上属于计算机与电子信息学院计算机协会。集训队由计算机与电子信息学院以学院的名义提供经费和场地支持，而集训队为学院以及学校提供程序设计竞赛（算法竞赛）和相关课程的支持、管理与维护。
+        </p>
+      </div>
+    </div>
+  </section>
+  <section
+    class="icpc-tech infoContainer"
+    style="text-align: right; margin-top: 20vh; margin-left: auto"
+    ref="icpcTechInfo"
+  >
+    <div class="info">
+      <div class="infoBrief" style="flex-direction: row-reverse">
+        <el-image class="item1" style="width: 60px; height: 60px" />
+        <div style="display: flex; flex-direction: column; gap: 5px">
+          <div class="text1">广西大学ICPC集训队技术组</div>
+          <div class="text2">ICPC Training Team of China, Guangxi University</div>
+        </div>
+      </div>
+      <div class="text3" style="margin-top: 2em; display: flex; flex-direction: column; gap: 0.5em">
+        <p>
+          广西大学ICPC集训队（ICPC Training Team of China, Guangxi
+          University）于2016年建立，2018-2019年开始逐渐正式运转，是一个为了代表广西大学参加以国际大学生程序设计竞赛（ICPC）、中国大学生程序设计竞赛（CCPC）等高水平赛事、希望与国内绝大部分院校接轨从而进行严格集中训练的队伍。集训队并非社团，仅为学生兴趣同好组织。集训队目前名义上属于计算机与电子信息学院计算机协会。集训队由计算机与电子信息学院以学院的名义提供经费和场地支持，而集训队为学院以及学校提供程序设计竞赛（算法竞赛）和相关课程的支持、管理与维护。集训队分为正式队伍和技术组：正式队员参加各地举行的算法竞赛，技术组为竞赛队员提供后勤服务，保障竞赛队员的训练正常开展，同时学习项目开发的前沿知识，参与队内开发活动。
+        </p>
+        <p>集训队并非社团，仅为学生兴趣同好组织。</p>
+
+        <p>
+          集训队目前名义上属于计算机与电子信息学院计算机协会。集训队由计算机与电子信息学院以学院的名义提供经费和场地支持，而集训队为学院以及学校提供程序设计竞赛（算法竞赛）和相关课程的支持、管理与维护。
+        </p>
+      </div>
+    </div>
+    <div class="line">
+      <div></div>
+    </div>
   </section>
   <section>
     <h1>集训队历年成果展示</h1>
   </section>
-
-  <section>
-    <h2>技术组简介</h2>
-    <p>这里放内容</p>
-    <p class="p_1">这里放内容</p>
-
-    <p>这里放内容</p>
-  </section>
-  <section>
-    <h1>这里放内容</h1>
-  </section>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+.headText {
+  min-height: 50vh;
+  font-size: 2em;
+  text-align: right;
+  word-spacing: 1em;
+  padding: 10%;
+  user-select: none;
+
+  .item1 {
+    height: 5em;
+  }
+  .item2 {
+    height: 2em;
+  }
+
+  .keyword {
+    font-size: 3em;
+  }
+}
+
+.infoContainer {
+  display: flex;
+  gap: 20px;
+  max-width: 60vw;
+  font-size: 1.4em;
+
+  .line > div {
+    width: 5px;
+    border-radius: 2.5px;
+    background-color: white;
+  }
+  .infoBrief {
+    display: flex;
+    gap: 20px;
+  }
+}
+</style>
