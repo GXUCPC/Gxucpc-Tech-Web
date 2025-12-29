5dcd9225e524a13d89c0d191fde616ea6c32ccb7
Joey Chan <Joey Chan>
2025-12-29 23:56:55 +0800

    feat-add-comment

--- README.md
+++ README.md
@@ -26,23 +26,11 @@ See [Vite Configuration Reference](https://vite.dev/config/).
 ## Project Setup
 
 ```sh
-pnpm install
+pnpm add -g vercel
 ```
 
 ### Compile and Hot-Reload for Development
 
 ```sh
-pnpm dev
-```
-
-### Type-Check, Compile and Minify for Production
-
-```sh
-pnpm build
-```
-
-### Lint with [ESLint](https://eslint.org/)
-
-```sh
-pnpm lint
+vercel dev
 ```
--- src/pages/other/contact.vue
+++ src/pages/other/contact.vue
@@ -1,43 +1,5 @@
-<script setup lang="ts">
-    import { onMounted, ref } from 'vue';
-    const giscusContainer = ref<HTMLElement | null>(null);
+<script></script>
 
-    onMounted(() => {
-        const script = document.createElement('script');
-        script.src = 'https://giscus.app/client.js';
-        script.setAttribute('data-repo', 'susitata/Gxu-Tch-Web-discussion');
-        script.setAttribute('data-repo-id', 'R_kgDOQuwzKA');
-        script.setAttribute('data-category', 'Announcements');
-        script.setAttribute('data-category-id', 'DIC_kwDOQuwzKM4C0O3S');
-        script.setAttribute('data-mapping', 'pathname');
-        script.setAttribute('data-strict', '0');
-        script.setAttribute('data-reactions-enabled', '1');
-        script.setAttribute('data-emit-metadata', '0');
-        script.setAttribute('data-input-position', 'bottom');
-        script.setAttribute('data-theme', 'preferred_color_scheme');
-        script.setAttribute('data-lang', 'zh-CN');
-        script.crossOrigin = 'anonymous';
-        script.async = true;
+<template></template>
 
-        const embedContainer = document.getElementById('giscus-embed');
-        if (embedContainer) {
-            embedContainer.appendChild(script);
-        }
-    });
-</script>
-
-<template>
-    <div class="giscus-container">
-        <div id="giscus-embed"></div>
-    </div>
-</template>
-
-<style scoped>
-    .giscus-container {
-        padding: 2rem;
-        max-width: 1200px;
-        margin : 2rem auto;
-        background: rgba(21, 21, 21, 0);
-        border-radius: 8px;
-    }
-</style>
+<style scoped></style>
